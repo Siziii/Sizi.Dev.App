57956f32e9847bd52b12a9505335bcaac400a45c
Siziii <sizi.dev@gmail.com>
2023-11-24 19:54:55 +0100

    Added filtering to Projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -23,7 +23,7 @@ const Project = ({thumbnail, title, group, link, paragraph,logo}) => {
 
                 <div className='flex flex-col justify-between sm:w-1/2 gap-5' style={{transformStyle: "preserve-3d"}}>
 
-                    <div className='flex gap-4 items-center'  style={{ transform: "translateZ(40px)" }}>
+                    <div className='flex gap-4  items-center'  style={{ transform: "translateZ(40px)" }}>
                         <div className='w-10 h-10 sm:w-12 sm:h-12 aspect-square rounded-full flex items-center justify-center bg-primary'>
                             <img src={logo} alt="logo" className='w-6 h-6 sm:w-7 sm:h-7' />
                         </div>
@@ -38,7 +38,7 @@ const Project = ({thumbnail, title, group, link, paragraph,logo}) => {
                     <button className="border-2 py-2 w-40 rounded-md hover:bg-white hover:text-gray03 transition-all"  style={{ transform: "translateZ(20px)" }}>Read more..</button>
                 </div>
                 
-                <div className='hidden sm:block w-1/2 aspect-square bg-gray02 rounded-md'  style={{ transform: "translateZ(20px)" }}>
+                <div className='hidden sm:block w-1/2 aspect-square bg-[#212122] rounded-md'  style={{ transform: "translateZ(20px)" }}>
                     {/* <img src={thumbnail} alt="" className='w-full aspect-square rounded-md' /> */}
                 </div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,17 @@
 import Project from "./Project";
 import projectData from "../data/projectData";
+import { useState } from "react";
 const Projects = () => {
+    const [isActive, setIsActive] = useState('all');
+
+    const filteredProjects = projectData.filter((project) => {
+        return isActive === 'all' || project.project_group === isActive;
+    })
+
+    const handleCategoryClick = (category) => {
+        setIsActive(category);
+    }
+
     return (
         <div className="w-[80%] h-full mx-auto">
             <div className="flex flex-col w-full h-full justify-center py-16">
@@ -9,9 +20,39 @@ const Projects = () => {
                     <h2 className="text-primary font-bold sm:text-2xl text-center">~ From Concept to Creation ~</h2>
                     <h1 className="font-black text-3xl sm:text-4xl md:text-5xl lg:text-7xl mb-8 uppercase text-center">Discover what I've built</h1>
                 </div>
-
+                <div className="w-full flex justify-center pb-10">
+                    <ul className="flex justify-center gap-1 sm:gap-3 text-sm sm:text-base font-bold ">
+                        <li
+                            className={`cursor-pointer opacity-50 hover:opacity-100 transition-all ${isActive === 'all' ? 'text-primary opacity-100' : ''}`}
+                            onClick={() => handleCategoryClick('all')}
+                        >
+                            All
+                        </li>
+                        <span className="opacity-50">/</span>
+                        <li
+                            className={`cursor-pointer opacity-50 hover:opacity-100 transition-all ${isActive === 'Web Development' ? 'text-primary opacity-100' : ''}`}
+                            onClick={() => handleCategoryClick('Web Development')}
+                        >
+                            Web Dev
+                        </li>
+                        <span className="opacity-50">/</span>
+                        <li
+                            className={` cursor-pointer opacity-50 hover:opacity-100 transition-all ${isActive === '3D Art' ? 'text-primary opacity-100' : ''}`}
+                            onClick={() => handleCategoryClick('3D Art')}
+                        >
+                            3D Art
+                        </li>
+                        <span className="opacity-50">/</span>
+                        <li
+                            className={`cursor-pointer opacity-50 hover:opacity-100 transition-all ${isActive === 'Game Development' ? 'text-primary opacity-100' : ''}`}
+                            onClick={() => handleCategoryClick('Game Development')}
+                        >
+                            Game Dev
+                        </li>
+                    </ul>
+                </div>
                 <div className="grid gap-4 grid-cols-1 xl:grid-cols-2">
-                    {projectData.map((project) => (
+                    {filteredProjects.map((project) => (
                         <Project
                             key={project.id}
                             thumbnail={project.thumbnail}
